37445c62d42836fb6c784f8dcffacad1d20d21c8
Antonio Mari <grasarade@gmail.com>
2023-01-21 00:37:18 +0100

    show common cases

--- source/bindings.py
+++ source/bindings.py
@@ -1,4 +1,5 @@
 import rdflib
+import pandas as pd
 from rdflib.plugins.sparql import prepareQuery
 
 # ds =
@@ -7,6 +8,7 @@ from rdflib.plugins.sparql import prepareQuery
 # ds3 =
 # ds4 = crimes
 
+"""
 g = rdflib.Graph()
 g.parse("dataset/complete.rdf")
 
@@ -20,3 +22,21 @@ for row in results:
     print(row)
 
 print(f"Num: {len(results)}")
+
+"""
+shoot_df: pd.DataFrame = pd.read_csv("../old_dataset/shoot.csv")
+arrest_1_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_1.csv")
+arrest_2_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_2.csv")
+
+arrest_case_codes = pd.concat([arrest_1_df["CASE NUMBER"], arrest_2_df["CASE NUMBER"]])
+print(f"Ci sono {len(arrest_case_codes)} casi di arresto")
+
+shoot_case_codes = shoot_df["CASE_NUMBER"]
+print(f"Ci sono {len(shoot_case_codes)} casi di spari")
+
+result = arrest_case_codes[arrest_case_codes.isin(shoot_case_codes)]
+print(f"Intersezione: {len(result)}")
+
+
+
+
