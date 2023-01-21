6327021edd3552ec219279188b6c0385d1f6e62b
Antonio Mari <grasarade@gmail.com>
2023-01-21 21:45:05 +0100

    Completed dataset csv binding

--- source/bindings.py
+++ source/bindings.py
@@ -24,10 +24,44 @@ for row in results:
 print(f"Num: {len(results)}")
 
 """
-shoot_df: pd.DataFrame = pd.read_csv("../old_dataset/shoot.csv")
-arrest_1_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_1.csv")
-arrest_2_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_2.csv")
 
+# BINDING SHOOT e ARREST
+shoot_df: pd.DataFrame = pd.read_csv("../old_dataset/shoot.csv")
+arrest_1_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_1.csv").rename({"CASE NUMBER": "CASE_NUMBER"}, axis=1)
+arrest_2_df: pd.DataFrame = pd.read_csv("../old_dataset/arrests_2.csv").rename({"CASE NUMBER": "CASE_NUMBER"}, axis=1)
+print(f"Shoot df {len(shoot_df.index)}")
+print(f"Arrest 1 df {len(arrest_1_df.index)}")
+print(f"Arrest 2 df {len(arrest_2_df.index)}")
+
+shoot_and_arrest_1_df: pd.DataFrame = shoot_df.merge(arrest_1_df, on="CASE_NUMBER")
+shoot_and_arrest_2_df: pd.DataFrame = shoot_df.merge(arrest_2_df, on="CASE_NUMBER")
+
+shoot_and_arrest: pd.DataFrame = pd.concat([shoot_and_arrest_1_df, shoot_and_arrest_2_df])
+
+print(len(shoot_and_arrest.index))
+
+# BINDING CON crimini
+crime_paths = ["../old_dataset/" + str(i) + ".csv" for i in range(2010, 2023)]
+complete_df = None
+
+for path in crime_paths:
+    print(f"File {path}")
+    crime_df: pd.DataFrame = pd.read_csv(path).rename({"Case Number": "CASE_NUMBER"}, axis=1)
+    print(f"\tLunghezza {len(crime_df.index)}")
+    if complete_df is None:
+        print("None")
+        complete_df = shoot_and_arrest.merge(crime_df, on="CASE_NUMBER")
+    else:
+        print("conc")
+        complete_df = pd.concat([complete_df, shoot_and_arrest.merge(crime_df, on="CASE_NUMBER")])
+
+    print(f"Lunghezza completo: {len(complete_df.index)}")
+
+print(len(complete_df.index))
+complete_df.to_csv("complete_crimes.csv")
+complete_df.describe().to_csv("report1.csv")
+
+"""
 arrest_case_codes = pd.concat([arrest_1_df["CASE NUMBER"], arrest_2_df["CASE NUMBER"]])
 print(f"Ci sono {len(arrest_case_codes)} casi di arresto")
 
@@ -36,6 +70,7 @@ print(f"Ci sono {len(shoot_case_codes)} casi di spari")
 
 result = arrest_case_codes[arrest_case_codes.isin(shoot_case_codes)]
 print(f"Intersezione: {len(result)}")
+"""
 
 
 
