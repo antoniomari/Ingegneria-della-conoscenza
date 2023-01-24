49ee88a270e42c86bb595366238e895fe95697df
rkmllh <w.mangione@studenti.uniba.it>
2023-01-24 19:50:46 +0100

    Updated kb

--- source/kb_usage.py
+++ source/kb_usage.py
@@ -4,7 +4,6 @@ def kb_usage():
 
     kb = Prolog()
     kb.consult("facts.pl")
-    kb.asserta("num_of_arrest(C, N) :- findall(S, has_arrest(C, S), L), length(L, N)")
     obj = kb.query("num_of_arrest(A, N)")
 
     for x in obj:
