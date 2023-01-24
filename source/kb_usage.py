e554e489f16670f31243093e059ff22b5bcfa419
rkmllh <w.mangione@studenti.uniba.it>
2023-01-24 14:15:24 +0100

    Fixed prolog kb

--- source/kb_usage.py
+++ source/kb_usage.py
@@ -4,16 +4,20 @@ import pytholog
 
 def kb_usage():
 
-    kb = pytholog.knowledge_base("facts.pl")
+    kb = pytholog.KnowledgeBase("k.pl")
+    kb.from_file("facts.pl")
 
-    kb(["same_district(C1, C2) :- district(C1, D), district(C2, D)",
-                "district(c1, d)",
-                "district(c2, d)",
-               "same_beat(C1, C2) :- beat(C1, B), beat(C2, B)",
-               "same_ward(C1, C2) :- ward(C1, W), ward(C2, W)",
-               "same_comm_area(C1, C2) :- comm_area(C1, COM), comm_area(C2, COM)"])
+    clauses = ["same_district(C1, C2) :- district(C1, D), district(C2, D).",
+               "same_beat(C1, C2) :- beat(C1, B), beat(C2, B).",
+               "same_ward(C1, C2) :- ward(C1, W), ward(C2, W).",
+               "same_comm_area(C1, C2) :- comm_area(C1, COM), comm_area(C2, COM).",
+               "num_of_same_district(C, N) :- findall(C1, same_district(C1, C), L), length(L, N)."]
 
-    print(kb.query(pytholog.Expr("same_district(X,Y)")))
+    kb(clauses)
+    obj = kb.query(pytholog.Expr("num_of_same_district(hs227745, N)"))
+
+    if obj is None:
+        pass # Do something
+    else:
+        print(obj)
     pass
-
-
