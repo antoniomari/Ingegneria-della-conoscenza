31b84e3bf82e6e2682e069505d242acf0b1e8f09
Antonio Mari <grasarade@gmail.com>
2023-01-25 23:21:14 +0100

    fix

--- source/my_kb.py
+++ source/my_kb.py
@@ -1,3 +1,6 @@
+import time
+
+import pandas as pd
 from pyswip import Prolog
 
 
@@ -6,30 +9,34 @@ def create_kb() -> Prolog:
 
     prolog.consult("facts.pl")
 
+    # CLAUSES ABOUT GEO-DATA
     prolog.assertz("same_district(C1, C2) :- district(C1, D), district(C2, D)")
     prolog.assertz("same_beat(C1, C2) :- beat(C1, B), beat(C2, B)")
     prolog.assertz("same_ward(C1, C2) :- ward(C1, W), ward(C2, W)")
     prolog.assertz("same_comm_area(C1, C2) :- comm_area(C1, COM), comm_area(C2, COM)")
     prolog.assertz("same_block(C1, C2) :- block(C1, B), block(C2, B)")
+    prolog.assertz("num_of_crimes_in_district(C, N) :- findall(C1, same_district(C, C1), L), length(L, N)")
+    prolog.assertz("num_of_crimes_in_beat(C, N) :- findall(C1, same_beat(C, C1), L), length(L, N)")
+    prolog.assertz("num_of_crimes_community_area(C, N) :- findall(C1, same_comm_area(C, C1), L), length(L, N)")
+    prolog.assertz("num_of_crimes_ward(C, N) :- findall(C1, same_ward(C, C1), L), length(L, N)")
+    prolog.assertz("num_of_crimes_block(C, N) :- findall(C1, same_block(C, C1), L), length(L, N)")
 
     prolog.assertz("criminal_arrested(C, P) :- has_arrest(C, A), arrested(A, P)")
-    prolog.assertz("is_ratial(C) :- criminal_arrested(C, P), victim(C, V), neq(race(V), race(P))")
+    prolog.assertz("is_ratial(C) :- criminal_arrested(C, P), victim(C, V), "
+                   "neq(victim_race(V, VR), criminal_race(P, PR))")
     prolog.assertz("same_zipcode(V1, V2) :- zip_code(V1, Z), zip_code(V2, Z)")
     prolog.assertz("crime_zip_code(C, Z) :- victimization(C, V, T), zip_code(V, Z)")
 
     prolog.assertz("income_area(C, I) :- comm_area(C, COM), income(COM, I)")
 
     prolog.assertz("num_of_victims(C, N) :- findall(V, victimization(C, V, T), L), length(L, N)")
-    prolog.assertz("num_of_crimes_in_district(C, N) :- findall(C1, same_district(C, C1), L), length(L, N)")
-    prolog.assertz("num_of_crimes_in_beat(C, N) :- findall(C1, same_beat(C, C1), L), length(L, N)")
-    prolog.assertz("num_of_crimes_community_area(C, N) :- findall(C1, same_comm_area(C, C1), L), length(L, N)")
-    prolog.assertz("num_of_crimes_ward(C, N) :- findall(C1, same_ward(C, C1), L), length(L, N)")
+
     prolog.assertz("num_of_dead(C, N) :- findall(V, victimization(C, V, homicide), L), length(L, N)")
     prolog.assertz("num_of_arrest(C, N) :- findall(S, has_arrest(C, S), L), length(L, N)")
     prolog.assertz("is_homicide(C) :- victimization(C, V, homicide)")
     prolog.assertz("is_killed(V) :- victimization(C, V, homicide)")
-    prolog.assertz("is_domestic(C) :- location_description(C, apartment); location_description(C, house); local_description(C, residence); local_description(C, driveway)")
-
+    prolog.assertz("is_domestic(C) :- location_description(C, apartment); location_description(C, house); "
+                   "local_description(C, residence); local_description(C, driveway)")
     prolog.assertz("night_crime(C) :- crime_date(C, datime(date(Y, M, D, H, M, S))), ((H >= 20; H =< 6))")
     prolog.assertz("crime_data_arrest(C, D) :- has_arrest(C, A), arrest_data(A, D)")
     prolog.assertz("immediate_arrest(C) :- crime_data(C, D), crime_data_arrest(C, D)") # or choose an error of date
@@ -57,11 +64,19 @@ def calculate_features(kb, crime_id) -> dict:
     features_dict["NUM_CRIMES_BEAT"] = list(kb.query(f"num_of_crimes_in_beat({crime_id}, N)"))[0]["N"]
     features_dict["NUM_CRIMES_COMM_AREA"] = list(kb.query(f"num_of_crimes_community_area({crime_id}, N)"))[0]["N"]
     features_dict["NUM_CRIMES_WARD"] = list(kb.query(f"num_of_crimes_ward({crime_id}, N)"))[0]["N"]
-
+    features_dict["NUM_CRIMES_BLOCK"] = list(kb.query(f"num_of_crimes_block({crime_id}, N)"))[0]["N"]
     return features_dict
 
 
+start = time.time()
+
 kb = create_kb()
-for crime_id in ["hs227745", "hs335199", "hs440368"]:
+for crime_id in pd.read_csv("crimes_selected.csv")["CASE_NUMBER"]:
     print(calculate_features(kb, crime_id))
+
+print(pd.read_csv("crimes_selected.csv")["CASE_NUMBER"])
+
+end = time.time()
+print("Time: ", end-start)
+
 
