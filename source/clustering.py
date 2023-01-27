4cd728a1fa8c21e54013c514e53e74b266bac900
rkmllh <w.mangione@studenti.uniba.it>
2023-01-27 21:09:07 +0100

    Embedding clustering

--- source/clustering.py
+++ source/clustering.py
@@ -11,36 +11,13 @@ import numpy
 def create_embedding():
 
     nltk.download('punkt')
-    df = pd.read_csv("shoot_selected.csv")
-    articles = []
-
-    for index, row in df.iterrows():
-
-        if pd.isnull(row["HOMICIDE_VICTIM_FIRST_NAME"]):
-            continue
-
-        try:
-            query = "Chicago homicide " + row['HOMICIDE_VICTIM_FIRST_NAME'] + " " + row['HOMICIDE_VICTIM_LAST_NAME']
-            news = WebDataPickUp(query, 3).pick_up()
-            articles.append(news.split())
-            print(articles)
-
-        except: pass
-        embedding = Embedding.build_embedding(articles)
-        print(embedding)
-        embedding_df: pd.DataFrame = pd.DataFrame(embedding)
-        embedding_df["VICTIM_CODE"] = row["VICTIM_CODE"]
-        embedding_df.to_csv("embeddings.csv", mode='a', index=False, header=False)
-
-        articles = []
-
-    my_embedding = Embedding.build_embedding(articles)
-    print(my_embedding)
-    pd.DataFrame(my_embedding).to_csv("pandasarray.csv")
+    df = pd.read_csv("embeddings.csv")
     wcss = []
-    
+    my_embedding = df.to_numpy().tolist()
+    print(type(my_embedding))
+
     # Trying to get best number of clusters
-    for i in range(1, 21):
+    for i in range(1, 3):
         clustering = KMeans(n_clusters = i, init = 'k-means++', random_state = 100)
         clustering.fit(my_embedding)
         wcss.append(clustering.inertia_)
