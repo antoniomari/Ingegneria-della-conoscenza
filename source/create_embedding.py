25e8156559e8b7547979aa362d3e83965ba2c063
Antonio Mari <grasarade@gmail.com>
2023-01-27 01:20:07 +0100

    completed code for create_embedding #3

--- source/create_embedding.py
+++ source/create_embedding.py
@@ -30,24 +30,8 @@ def create_embedding():
         print(embedding)
         embedding_df: pd.DataFrame = pd.DataFrame(embedding)
         embedding_df["VICTIM_CODE"] = row["VICTIM_CODE"]
-        embedding_df.to_csv("embeddings.csv", mode='a', index=False)
+        embedding_df.to_csv("embeddings.csv", mode='a', index=False, header=False)
 
         articles = []
 
-    my_embedding = Embedding.build_embedding(articles)
-    print(my_embedding)
-    pd.DataFrame(my_embedding).to_csv("pandasarray.csv")
-    wcss = []
-    
-    # Trying to get best number of clusters
-    for i in range(1, 21):
-        clustering = KMeans(n_clusters = i, init = 'k-means++', random_state = 100)
-        clustering.fit(my_embedding)
-        wcss.append(clustering.inertia_)
-
-    ks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    print(ks, wcss)
-    matplotlib.pyplot.plot(ks, wcss)
-    matplotlib.pyplot.show()
-
-    pass
+create_embedding()
