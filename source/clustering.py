98cc859e0834d58a0777461c7c65b3ba4f72e7bc
rkmllh <w.mangione@studenti.uniba.it>
2023-01-27 21:34:01 +0100

    Created Clustering

--- source/clustering.py
+++ source/clustering.py
@@ -13,17 +13,17 @@ def create_embedding():
     nltk.download('punkt')
     df = pd.read_csv("embeddings.csv")
     wcss = []
-    my_embedding = df.to_numpy().tolist()
-    print(type(my_embedding))
+    my_embedding = df.drop(["VICTIM_CODE"], axis=1).to_numpy().tolist()
 
     # Trying to get best number of clusters
-    for i in range(1, 3):
-        clustering = KMeans(n_clusters = i, init = 'k-means++', random_state = 100)
+    for i in range(1, 21):
+        clustering = KMeans(n_clusters = i, init = 'k-means++', random_state = 100, n_init=10)
         clustering.fit(my_embedding)
         wcss.append(clustering.inertia_)
 
     ks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     print(ks, wcss)
+    matplotlib.pyplot.xticks(ks)
     matplotlib.pyplot.plot(ks, wcss)
     matplotlib.pyplot.show()
 
